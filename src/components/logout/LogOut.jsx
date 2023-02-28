b31cb54e3e8d50a7408eb5e67dec028c46040209
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-02-28 16:56:44 +0200

    am facut ca modalul sa fie inschis din esc si din click pe langa modal

--- src/components/logout/LogOut.jsx
+++ src/components/logout/LogOut.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "./LogIn.css";
+import "../login/LogIn.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function LogInAuth() {
-  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
 
   return (
     <div className="login-container">
-      <button className="button-login" onClick={() => loginWithRedirect()}>
-        Log In
+      <button className="button-login" onClick={() => logout()}>
+        Log Out
       </button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
-import LogIn from "./components/login/LogIn";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Auth0Provider
-    domain="dev-0s4arhgkddn0wys2.us.auth0.com"
-    clientId="U5thR1xXobWZ3MvTu10b7yGq0Pr4G9l7"
-    redirectUri={window.location.origin}
-  >
-    <React.StrictMode>
-      <LogIn>
-        <App />
-      </LogIn>
-    </React.StrictMode>
-  </Auth0Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
